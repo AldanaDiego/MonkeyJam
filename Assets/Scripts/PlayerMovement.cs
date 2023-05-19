629f9da8ddb40abbcbc0c6494c5e35373dc583ce
Diego Aldana <daldana9@gmail.com>
2023-05-19 10:54:00 -0400

    Player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Transform _propulsors;
+    [SerializeField] private float _movementSpeed;
+    [SerializeField] private float _rotationSpeed;
+    [SerializeField] private float _brakeSpeed;
+    [SerializeField] private float _maxSpeed;
+    [SerializeField] private float _boundsOffset;
+
+    private Transform _transform;
+    private InputManager _inputManager;
+    private Rigidbody _rigidBody;
+    private Vector2 _screenBounds;
+
+    private void Start()
+    {
+        _transform = transform;
+        _inputManager = InputManager.GetInstance();
+        _screenBounds = ScreenBoundary.GetInstance().GetScreenBounds();
+        Debug.Log(_screenBounds);
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Vector2 movInput = _inputManager.GetMovementInput();
+        if (movInput != Vector2.zero)
+        {
+            HandleAcceleration(movInput.y);
+            HandleRotation(movInput.x);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 pos = _transform.position;
+        pos.x = 0.0f;
+        pos.z = Mathf.Clamp(pos.z, (_screenBounds.x * -1.0f) + _boundsOffset, _screenBounds.x - _boundsOffset);
+        pos.y = Mathf.Clamp(pos.y, (_screenBounds.y * -1.0f) + _boundsOffset, _screenBounds.y - _boundsOffset);
+        _transform.position = pos;
+    }
+
+    private void HandleAcceleration(float value)
+    {
+        if (value == 0.0f)
+        {
+            return;
+        }
+        else if (value > 0.0f) //Accelerate
+        {
+            Vector3 direction = _propulsors.up * (_movementSpeed * Time.deltaTime);
+            _rigidBody.AddForce(direction, ForceMode.Force);
+            if (_rigidBody.velocity.magnitude > _maxSpeed)
+            {
+                _rigidBody.velocity = _rigidBody.velocity.normalized * _maxSpeed;
+            }
+        }
+        else //Brake
+        {
+            if (_rigidBody.velocity.magnitude > 0.1f)
+            {
+                _rigidBody.AddForce(_rigidBody.velocity * (_brakeSpeed * -1.0f), ForceMode.Force);
+            }
+        }
+    }
+
+    private void HandleRotation(float value)
+    {
+        if (value == 0.0f)
+        {
+            return;
+        }
+
+        float rotation = _rotationSpeed * Time.deltaTime * (value > 0.0f ? 1.0f : -1.0f);
+        _propulsors.Rotate(0.0f, 0.0f, rotation);
+    }
+
+}
---
+++ Assets/Scripts/ScreenBoundary.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenBoundary : Singleton<ScreenBoundary>
+{
+    [SerializeField] private Camera _mainCamera;
+    private Vector2 _screenBounds;
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    private void Start()
+    {
+        _screenBounds = new Vector2(
+            _mainCamera.orthographicSize * Screen.width / Screen.height,
+            _mainCamera.orthographicSize
+        );
+    }
+
+    public Vector2 GetScreenBounds()
+    {
+        return _screenBounds;
+    }
+}
