d9a2f292674476d0f4813d83103f41b121e3820d
Diego Aldana <daldana9@gmail.com>
2023-05-19 09:27:27 -0400

    Base input system and player prefab

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : Singleton<InputManager>
+{
+    private PlayerInputActions _playerInput;
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    private void Start()
+    {
+        _playerInput = new PlayerInputActions();
+        _playerInput.Enable();
+    }
+
+    public Vector2 GetMovementInput()
+    {
+        return _playerInput.Player.Move.ReadValue<Vector2>();
+    }
+    
+}
---
+++ Assets/Scripts/Utils/Singleton.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    protected static T Instance;
+
+    protected virtual void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Debug.Log($"Repeated instance of {typeof(T)}");
+            Destroy(this);
+            return;
+        }
+        Instance = GetComponent<T>();
+    }
+
+    public static T GetInstance()
+    {
+        if (Instance == null)
+        {
+            Debug.Log($"Instance of {typeof(T)} is null, searching...");
+            Instance = FindObjectOfType<T>();
+        }
+        return Instance;
+    }
+}
