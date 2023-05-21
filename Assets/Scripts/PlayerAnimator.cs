578cea1c074d281d9d905974a5b3e7149e8225d2
Diego Aldana <daldana9@gmail.com>
2023-05-20 22:10:31 -0400

    Improved monkey animation

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -10,13 +10,33 @@ public class PlayerAnimator : MonoBehaviour
     [SerializeField] private ParticleSystem _smoke;
     [SerializeField] private Animator _monkeyAnimator;
 
+    private bool _active;
+    private float _changeDanceCooldown = 3f;
+    private float _changeDanceTimer;
+
     private void Start()
     {
         PlayerHealth.OnPlayerDeath += OnPlayerDeath;
+        _active = true;
+        _changeDanceTimer = 0f;
+    }
+
+    private void Update()
+    {
+        if (_active)
+        {
+            _changeDanceTimer += Time.deltaTime;
+            if (_changeDanceTimer > _changeDanceCooldown)
+            {
+                _changeDanceTimer = 0;
+                _monkeyAnimator.SetInteger("DanceStyle", UnityEngine.Random.Range(1, 4));
+            }
+        }
     }
 
     private void OnPlayerDeath(object sender, EventArgs empty)
     {
+        _active = false;
         Destroy(_vessel);
         Destroy(_propulsors);
         _smoke.Play();
